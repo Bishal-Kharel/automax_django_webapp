cd5ead9751cdbe7d17616950c8d6f6e3287d631d
Bishal Kharel <bishalkharel@Bishals-MacBook-Pro.local>
2025-04-24 14:31:17 +0545

    Fixed phone number and cleaned codes

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from mailbox import Message
 import os
 import environ
 from pathlib import Path
